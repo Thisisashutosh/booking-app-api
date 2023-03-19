e87be62ea66fc3c17e5afad2235b593de4062642
Ashutosh <thisisashutoshdubey@gmail.com>
2023-03-19 18:08:33 +0530

    second commit

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,24 @@
 import express from "express"
+import { deleteUser, getUser, getUsers, updateUser } from '../controllers/user.js'
+import { verifyToken } from "../utils/verifyToken.js"
 
 const router=express.Router()
 
+router.get("/checkauthentication",verifyToken,(req,res,next)=>{
+    res.send("Welcome")
+})
+
+router.get("/checkuser",verifyToken,(req,res,next)=>{
+    res.send("Welcome! Now you can delete your account")
+})
+
+//Update
+router.put('/:id', updateUser)
+//Delete
+router.delete('/:id',deleteUser)
+//Get
+router.get('/:id', getUser)
+//Get all
+router.get('/', getUsers)
 
 export default router
